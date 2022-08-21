6eacd584324cc586894647c00435a5683d842beb
Aliabbas789 <aliabbasattarwala41@gmail.com>
2022-08-21 12:14:33 +0530

    Initial Commit

---
+++ admin/partials/footer.php
@@ -0,0 +1,13 @@
+<!-- Footer section starts -->
+<div class="footer">
+    <div class="wrapper">
+        <p class="text-center">
+            All rights reserved.Food House. Designed by
+            <a href="#">Aliabbas</a>
+        </p>
+    </div>
+</div>
+<!-- Footer section ends -->
+</body>
+
+</html>
---
+++ admin/partials/menu.php
@@ -0,0 +1,31 @@
+<html>
+
+<head>
+    <title>Food Order Website - Home Page</title>
+    <link rel="stylesheet" href="../css/admin.css">
+</head>
+
+<body>
+    <!-- Menu section starts -->
+    <div class=" menu text-center ">
+        <div class="wrapper ">
+            <ul>
+                <li>
+                    <a class="menu-link" href="index.php ">Home</a>
+                </li>
+                <li>
+                    <a class="menu-link" href="manage-admin.php ">Admin</a>
+                </li>
+                <li>
+                    <a class="menu-link" href="manage-category.php ">Category</a>
+                </li>
+                <li>
+                    <a class="menu-link" href="manage-food.php ">Food</a>
+                </li>
+                <li>
+                    <a class="menu-link" href="manage-order.php ">Order</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <!-- Menu section ends -->
